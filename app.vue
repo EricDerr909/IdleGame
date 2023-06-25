29d50d3d7b15c21221a5e7a53c952238dd2692f1
Eric Derr <ericderr909@gmail.com>
2023-06-25 15:15:02 -0500

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <pre>
+  || ______________________________________________________________________________________ ||
+  ||                          ________                   __                                 ||
+  ||                          \______ \   __ __   ____  |  | __                             ||
+  ||                           |    |  \ |  |  \_/ ___\ |  |/ /                             ||
+  ||                           |    `   \|  |  /\  \___ |    \                              ||
+  ||                          /_______  /|____/  \___  >|__|_ \                             ||
+  ||                                  \/             \/      \/                             ||
+  ||                           _____                                                        ||
+  ||                          /  _  \  _______   _____   ___.__.                            ||
+  ||                         /  /_\  \ \_  __ \ /     \ \   |  |                            ||
+  ||                        /    |    \ |  | \/|  Y Y  \ \___  |                            ||
+  ||                        \____|__  / |__|   |__|_|  / / ____|                            ||
+  ||                                \/               \/  \/                                 ||
+  || ______________________________________________________________________________________ ||
+  <br/>
+
+
+    Eggs: {{ eggCount }}          Ducks: {{ duckCount }} <br/>
+    <button @click="lay()">Lay Egg</button>           <button @click="hatch()">Hatch Egg</button>
+
+  </pre>
+  </div>
+</template>
+
+<script setup>
+  let eggCount = ref(0);
+  let duckCount = ref(1);
+
+  function lay(){
+    eggCount.value += duckCount.value;
+  }
+
+  function hatch(){
+    if(eggCount.value > 0){
+      duckCount.value += 1;
+      eggCount.value -= 1;
+    }
+  }
+</script>
